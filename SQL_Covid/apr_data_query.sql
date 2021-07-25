780803831ec59092792f3fa59bcead3e45cd7f95
Tophurkey <dualgatling@gmail.com>
2021-07-24 21:28:42 -0700

    first sql exploration of covid
    looked at april cases

---
+++ SQL_Covid/apr_data_query.sql
@@ -0,0 +1,12 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT apr_data.location, sum_new_cases, sum_tot_cases--, sum_new_death, sum_tot_death
+FROM 
+(SELECT TOP (1000)
+      [location]
+      ,SUM([total_cases]) as sum_tot_cases
+      ,SUM(new_cases) as sum_new_cases
+      --,SUM([total_deaths]) as sum_tot_death
+      --,SUM([new_deaths]) as sum_new_death
+  FROM [ProjectsDatabase].[dbo].[CovidMaster]
+  WHERE date LIKE '2020-04-%' AND continent IS NOT NULL
+  GROUP BY location) AS apr_data
